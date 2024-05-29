61bf1d4b8aadea8524f2a2166bf3950afb101d8d
Emiliano Vera Bouvier <emiivera077@gmail.com>
2024-05-29 20:04:45 -0300

    Agregar archivos de proyecto.

---
+++ ObligatorioMVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ObligatorioMVC.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> 
+            options) : base (options) { }
+    }
+}
