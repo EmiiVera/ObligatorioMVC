7ad77fd534000c76d266ce6dd755dd9237e9e417
hen1990 <hen1990@hotmail.es>
2024-06-05 18:33:35 -0300

    update clase maquina

--- ObligatorioMVC/Models/Maquina.cs
+++ ObligatorioMVC/Models/Maquina.cs
@@ -7,6 +7,13 @@
         public double precioCompra { get; set; }
         public string disponibilidad { get; set; }
         public TipoMaquina tipoMaquina { get; set; }
+        public DateTime vidaUtil { get; set; }
 
+        public DateTime vidaUtilRestante()
+        {
+            DateTime dateTime = DateTime.Now;
+
+            return vidaUtil;
+        }
     }
 }
