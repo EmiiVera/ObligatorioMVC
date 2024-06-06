82e14ea27cdbbd44b9303613196e0a82c7c8be6d
Alumno Henry <Alumno@DESKTOP-LD9AVJP>
2024-06-05 21:43:00 -0300

    stringLength a clase local

--- ObligatorioMVC/Models/Local.cs
+++ ObligatorioMVC/Models/Local.cs
@@ -6,6 +6,7 @@
         public int IdLocal { get; set; }
         [Required]
         [Display(Name = "Local")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 50 caracteres.")]
         public string NombreLocal { get; set; }
         public string Ciudad { get; set; }
         public string Direccion { get; set; }
