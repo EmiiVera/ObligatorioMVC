487d1aa4e2a37e7229277f8cac320ce927cb411f
hen1990 <hen1990@hotmail.es>
2024-06-13 18:38:51 -0300

    CRUD locales, maquina, socio, responsable

---
+++ ObligatorioMVC/Controllers/SociosController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ObligatorioMVC.Datos;
+using ObligatorioMVC.Models;
+
+namespace ObligatorioMVC.Controllers
+{
+    public class SociosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SociosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Socios
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Socio.Include(s => s.local);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Socios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var socio = await _context.Socio
+                .Include(s => s.local)
+                .FirstOrDefaultAsync(m => m.IdUsuario == id);
+            if (socio == null)
+            {
+                return NotFound();
+            }
+
+            return View(socio);
+        }
+
+        // GET: Socios/Create
+        public IActionResult Create()
+        {
+            ViewData["IdLocal"] = new SelectList(_context.Local, "IdLocal", "Ciudad");
+            return View();
+        }
+
+        // POST: Socios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdLocal,IdUsuario,NombreUsuario,TelefonoUsuario")] Socio socio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(socio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdLocal"] = new SelectList(_context.Local, "IdLocal", "Ciudad", socio.IdLocal);
+            return View(socio);
+        }
+
+        // GET: Socios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var socio = await _context.Socio.FindAsync(id);
+            if (socio == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdLocal"] = new SelectList(_context.Local, "IdLocal", "Ciudad", socio.IdLocal);
+            return View(socio);
+        }
+
+        // POST: Socios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdLocal,IdUsuario,NombreUsuario,TelefonoUsuario")] Socio socio)
+        {
+            if (id != socio.IdUsuario)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(socio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SocioExists(socio.IdUsuario))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdLocal"] = new SelectList(_context.Local, "IdLocal", "Ciudad", socio.IdLocal);
+            return View(socio);
+        }
+
+        // GET: Socios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var socio = await _context.Socio
+                .Include(s => s.local)
+                .FirstOrDefaultAsync(m => m.IdUsuario == id);
+            if (socio == null)
+            {
+                return NotFound();
+            }
+
+            return View(socio);
+        }
+
+        // POST: Socios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var socio = await _context.Socio.FindAsync(id);
+            if (socio != null)
+            {
+                _context.Socio.Remove(socio);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SocioExists(int id)
+        {
+            return _context.Socio.Any(e => e.IdUsuario == id);
+        }
+    }
+}
